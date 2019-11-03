70cb3f0cad0118025cec6f290517e0b3f09314fa
jw20191n <clairejw1015@gmail.com>
2019-11-03 18:12:33 -0500

    chn.js backtotop btn added

--- js/chn.js
+++ js/chn.js
@@ -63,3 +63,24 @@ function showmore(){
     }
 
 }
+
+//======================  back to top button ========================
+const topBtn = document.getElementById('backToTop');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener('scroll', ()=>{
+    scrollFunction();
+})
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
